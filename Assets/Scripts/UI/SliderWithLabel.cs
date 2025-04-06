5030986967cec7a3812eb539c1dc06f0a565345c
Kat <kxh8057@g.rit.edu>
2025-04-06 16:04:50 -0400

    sliders show value change

---
+++ Assets/Scripts/UI/SliderWithLabel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderWithLabel : MonoBehaviour { 
+
+    public Slider slider;
+    public TextMeshProUGUI label;
+
+    public string formatString = "{0}<size=90%>{1}<size=75%>%";
+    public bool isPercentBased;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.onValueChanged.AddListener(UpdateLabel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        slider.onValueChanged.RemoveListener(UpdateLabel);
+    }
+
+    public void UpdateLabel(float val)
+    {
+        string hundredsPlace;
+        string tensOnesPlace;
+        int percent;
+        if (isPercentBased)
+        {
+            //if the values on this slider already represent a percent, just reformat
+            percent = Mathf.RoundToInt(val * 100);
+
+        }
+        else
+        {
+            //otherwise, map values to percent based on min and max
+            float range = slider.maxValue - slider.minValue;
+            percent = Mathf.RoundToInt(( (val - slider.minValue) / range) * 100);
+        }
+
+        int hundreds = Mathf.FloorToInt(percent / 100);
+        if (hundreds > 0) { hundredsPlace = hundreds.ToString("0"); } else { hundredsPlace = ""; }
+        tensOnesPlace = (percent % 100).ToString("00");
+
+        label.text = string.Format(formatString, hundredsPlace, tensOnesPlace);
+    }
+}
