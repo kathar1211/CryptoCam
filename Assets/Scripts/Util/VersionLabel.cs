21c4f9e47556d09abca1ff69dc23cdf3f32ab543
Kat <kxh8057@g.rit.edu>
2026-01-09 08:47:51 -0500

    creating initial version of prompt for error tracking

---
+++ Assets/Scripts/Util/VersionLabel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionLabel : MonoBehaviour
+{
+    public TextMeshProUGUI Label;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Label.text = "version " + Application.version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
